0578fe0501e1d60fe51c9c7b75eb9a0638625cb2
FirstJava <70321748@qq.com>
2020-05-22 14:13:00 +0800

    新建csp后段管理系统品质清机功能

---
+++ csp-back-management/src/main/java/com/csp/controller/BackMagagementApplication.java
@@ -0,0 +1,21 @@
+package com.csp.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：FirstJava
+ * @date ：Created in 2020/5/22 11:39
+ * @description：r
+ * @version: 1.0
+ * @Copyright © 2020 by FirstJava
+ */
+
+@SpringBootApplication
+public class BackMagagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackMagagementApplication.class, args);
+    }
+
+}
